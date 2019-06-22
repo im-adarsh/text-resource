2749331d0a0b7576fb16fa7d786ecc81c1c3d700
im-adarsh <adarshkumar9820@gmail.com>
2019-06-22 22:10:24 +0800

    adding text resource client

--- text-resource/service/service.go
+++ text-resource/service/service.go
@@ -1,1 +1,15 @@
 package service
+
+type textResourceClient struct{}
+
+func NewTextResourceClient()TextResourceClient {
+	return &textResourceClient{}
+}
+
+func (textResourceClient) GetText(GetTextRequest) string {
+	panic("implement me")
+}
+
+func (textResourceClient) AddText(AddTextRequest) error {
+	panic("implement me")
+}
--- text-resource/service/types.go
+++ text-resource/service/types.go
@@ -1,1 +1,18 @@
 package service
+
+type GetTextRequest struct {
+	textId string
+	locale string
+	params []string
+}
+
+type AddTextRequest struct {
+	textId string
+	locale string
+	displayTextTmpl string
+}
+
+type TextResourceClient interface {
+	GetText(GetTextRequest) string
+	AddText(AddTextRequest) error
+}
