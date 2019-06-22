7e66bfc7f21998aa04398cb2850e765eebf9d87b
im-adarsh <adarshkumar9820@gmail.com>
2019-06-22 23:44:47 +0800

    adding text resource client

--- text-resource/cmd/server/main.go
+++ text-resource/cmd/server/main.go
@@ -3,11 +3,17 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/im-adarsh/text-resource/text-resource/cmd/server/endpoint"
+	"github.com/im-adarsh/text-resource/text-resource/service"
 )
 
 func main() {
+
+	// init
+	trClient := service.NewTextResourceClient()
+	// create gin server
 	r := gin.Default()
-	endpoint.RegisterEndPoint(r)
+	endpoint.MakeGetTextEndpoint(trClient)
+	endpoint.RegisterEndPoint(r, trClient)
 	err := r.Run() // listen and serve on 0.0.0.0:8080
 	if err != nil {
 		panic("unable to run server")
--- text-resource/text_resource_proto/generate.sh
+++ text-resource/text_resource_proto/generate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "generating proto"
+echo "generating text_resource_proto"
 protoc -I . text_resource.proto --go_out=plugins=http:.
 sed -i "" "s/,omitempty//" text_resource.pb.go
