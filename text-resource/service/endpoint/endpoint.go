72e500eeb67d5260cd425b82053b1f9c92f62c82
Adarsh Kumar <adarshkumar9820@gmail.com>
2019-08-18 01:28:46 +0800

    endpoint group

--- text-resource/service/endpoint/endpoint.go
+++ text-resource/service/endpoint/endpoint.go
@@ -10,31 +10,35 @@ import (
 const endpointPrefix = "/text-resource/v1"
 
 const (
-	Load    = endpointPrefix + "/load"
-	GetText = endpointPrefix + "/get-text/:locale/:key"
+	Load    = "/load"
+	GetText = "/get-text/:locale/:key"
 )
 
 func MakeEndPoint(r *gin.Engine, t service.TranslationService) {
 
-	r.GET(Load, func(c *gin.Context) {
-		err := t.Load()
-		if err != nil {
-			fmt.Println("", err)
-		}
-		c.JSON(200, gin.H{
-			"message": "pong",
+	v1 := r.Group(endpointPrefix)
+	{
+		v1.GET(Load, func(c *gin.Context) {
+			err := t.Load()
+			if err != nil {
+				fmt.Println("", err)
+			}
+			c.JSON(200, gin.H{
+				"message": "pong",
+			})
 		})
-	})
 
-	r.GET(GetText, func(c *gin.Context) {
-		locale := c.Param("locale")
-		key := c.Param("key")
-		s, err := t.GetText(key, locale)
-		if err != nil {
-			fmt.Println("", err)
-		}
-		c.JSON(200, gin.H{
-			"message": s,
+		v1.GET(GetText, func(c *gin.Context) {
+			locale := c.Param("locale")
+			key := c.Param("key")
+			s, err := t.GetText(key, locale)
+			if err != nil {
+				fmt.Println("", err)
+			}
+			c.JSON(200, gin.H{
+				"message": s,
+			})
 		})
-	})
+	}
+
 }
